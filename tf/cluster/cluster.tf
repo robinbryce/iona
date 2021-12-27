9bd90a700575fdee57f34b5fb7c244872060187e
Robin Bryce <robinbryce@gmail.com>
2021-12-27 16:46:35 +0000

    n2-standard-4 -> n2-standard-2 because I am quotad to 24 CPU's

--- tf/cluster/cluster.tf
+++ tf/cluster/cluster.tf
@@ -70,7 +70,7 @@ module "cluster" {
       service_account    = "kluster-serviceaccount@${var.gcp_project_id}.iam.gserviceaccount.com"
     }
     work-pool = {
-      machine_type       = "n2-standard-4" # $$$
+      machine_type       = "n2-standard-2" # $$$
       initial_node_count = 3
       min_node_count     = 1
       max_node_count     = 16
