357926533eca4f7ce371e9203d9135be2647ebfe
Robin Bryce <robinbryce@gmail.com>
2021-12-28 15:37:08 +0000

    adding identity platform support

---
+++ tf/authn/main.tf
@@ -0,0 +1,50 @@
+variable "cluster_workspace" {
+  type = string
+  default = "cluster"
+}
+output "cluster_workspace" { value = var.cluster_workspace }
+
+variable "cluster_name" {
+    type = string
+    default = "kluster"
+}
+output "cluster_name" { value = var.cluster_name }
+
+locals {
+  # All remote state references are via variables with short cuts in the
+  # locals.
+  gcp_project_id = data.terraform_remote_state.cluster.outputs.gcp_project_id
+  gcp_project_region = data.terraform_remote_state.cluster.outputs.gcp_project_region
+  gcp_project_zone = data.terraform_remote_state.cluster.outputs.gcp_project_zone
+  static_ingress = data.terraform_remote_state.cluster.outputs.static_ingress
+  # this is the workload identity base for the cluster. All workload identities
+  # are constructed from this - thats how they work.
+  workloadid_fqdn = "serviceAccount:${data.terraform_remote_state.cluster.outputs.gcp_project_id}.svc.id.goog"
+}
+
+data "terraform_remote_state" "cluster" {
+  backend = "remote"
+  config = {
+    organization = "robinbryce"
+    workspaces = {
+      # name = "iona-1"
+      name = var.cluster_workspace
+    }
+  }
+}
+
+data "google_client_config" "provider" {}
+data "google_container_cluster" "project" {
+  name = var.cluster_name
+  project = data.terraform_remote_state.cluster.outputs.gcp_project_id
+  location = data.terraform_remote_state.cluster.outputs.gcp_project_zone
+}
+
+provider "kubernetes" {
+  load_config_file = "false"
+  host = "https://${data.google_container_cluster.project.endpoint}"
+  token = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.project.master_auth[0].cluster_ca_certificate,
+  )
+}
---
+++ tf/authn/terraform.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "~> 1.1.2"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.5.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google"
+      version = "~> 4.5.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "robinbryce"
+    workspaces {
+      name = "iona-authn"
+    }
+  }
+}
