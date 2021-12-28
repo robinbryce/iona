8354a8ac0162c0320fc36bb376894a722c957fee
Robin Bryce <robinbryce@gmail.com>
2021-12-28 23:04:56 +0000

    adding traefik crds

--- tf/cluster-k8s/traefik-crds.tf
+++ tf/cluster-k8s/traefik-crds.tf
@@ -1,19 +1,37 @@
 resource "kubernetes_manifest" "ingressroutes" {
   manifest = {
-    apiVersion = "v1"
+    apiVersion = "apiextensions.k8s.io/v1"
     kind = "CustomResourceDefinition"
     metadata = {
       name = "ingressroutes.traefik.containo.us"
     }
     spec = {
       group = "traefik.containo.us"
-      version = "v1alpha1"
       scope = "Namespaced"
       names = {
         kind = "IngressRoute"
         plural = "ingressroutes"
         singular = "ingressroute"
       }
+
+      versions = [{
+        name    = "v1alpha1"
+        served  = true
+        storage = true
+        schema = {
+          openAPIV3Schema = {
+            type = "object"
+            properties = {
+              data = {
+                type = "string"
+              }
+              refs = {
+                type = "number"
+              }
+            }
+          }
+        }
+      }]
     }
   }
 }
