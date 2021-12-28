1bce7c5eb25ae8e41447fbf2a5eaac4f0ad80acb
Robin Bryce <robinbryce@gmail.com>
2021-12-28 23:12:51 +0000

    adding traefik crds

--- tf/cluster-k8s/traefik-crds.tf
+++ tf/cluster-k8s/traefik-crds.tf
@@ -35,3 +35,231 @@ resource "kubernetes_manifest" "ingressroutes" {
     }
   }
 }
+
+resource "kubernetes_manifest" "middlewares" {
+  manifest = {
+    apiVersion = "apiextensions.k8s.io/v1"
+    kind = "CustomResourceDefinition"
+    metadata = {
+      name = "middlewares.traefik.containo.us"
+    }
+    spec = {
+      group = "traefik.containo.us"
+      scope = "Namespaced"
+      names = {
+        kind = "Middleware"
+        plural = "middlewares"
+        singular = "middleware"
+      }
+
+      versions = [{
+        name    = "v1alpha1"
+        served  = true
+        storage = true
+        schema = {
+          openAPIV3Schema = {
+            type = "object"
+            properties = {
+              data = {
+                type = "string"
+              }
+              refs = {
+                type = "number"
+              }
+            }
+          }
+        }
+      }]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "ingressroutetcps" {
+  manifest = {
+    apiVersion = "apiextensions.k8s.io/v1"
+    kind = "CustomResourceDefinition"
+    metadata = {
+      name = "ingressroutetcps.traefik.containo.us"
+    }
+    spec = {
+      group = "traefik.containo.us"
+      scope = "Namespaced"
+      names = {
+        kind = "IngressRouteTCP"
+        plural = "ingressroutetcps"
+        singular = "ingressroutetcp"
+      }
+
+      versions = [{
+        name    = "v1alpha1"
+        served  = true
+        storage = true
+        schema = {
+          openAPIV3Schema = {
+            type = "object"
+            properties = {
+              data = {
+                type = "string"
+              }
+              refs = {
+                type = "number"
+              }
+            }
+          }
+        }
+      }]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "ingressrouteudps" {
+  manifest = {
+    apiVersion = "apiextensions.k8s.io/v1"
+    kind = "CustomResourceDefinition"
+    metadata = {
+      name = "ingressrouteudps.traefik.containo.us"
+    }
+    spec = {
+      group = "traefik.containo.us"
+      scope = "Namespaced"
+      names = {
+        kind = "IngressRouteUDP"
+        plural = "ingressrouteudps"
+        singular = "ingressrouteudp"
+      }
+
+      versions = [{
+        name    = "v1alpha1"
+        served  = true
+        storage = true
+        schema = {
+          openAPIV3Schema = {
+            type = "object"
+            properties = {
+              data = {
+                type = "string"
+              }
+              refs = {
+                type = "number"
+              }
+            }
+          }
+        }
+      }]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "tlsoptions" {
+  manifest = {
+    apiVersion = "apiextensions.k8s.io/v1"
+    kind = "CustomResourceDefinition"
+    metadata = {
+      name = "tlsoptions.traefik.containo.us"
+    }
+    spec = {
+      group = "traefik.containo.us"
+      scope = "Namespaced"
+      names = {
+        kind = "TLSOption"
+        plural = "tlsoptions"
+        singular = "tlsoption"
+      }
+
+      versions = [{
+        name    = "v1alpha1"
+        served  = true
+        storage = true
+        schema = {
+          openAPIV3Schema = {
+            type = "object"
+            properties = {
+              data = {
+                type = "string"
+              }
+              refs = {
+                type = "number"
+              }
+            }
+          }
+        }
+      }]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "tlsstores" {
+  manifest = {
+    apiVersion = "apiextensions.k8s.io/v1"
+    kind = "CustomResourceDefinition"
+    metadata = {
+      name = "tlsstores.traefik.containo.us"
+    }
+    spec = {
+      group = "traefik.containo.us"
+      scope = "Namespaced"
+      names = {
+        kind = "TLSStore"
+        plural = "tlsstores"
+        singular = "tlsstore"
+      }
+
+      versions = [{
+        name    = "v1alpha1"
+        served  = true
+        storage = true
+        schema = {
+          openAPIV3Schema = {
+            type = "object"
+            properties = {
+              data = {
+                type = "string"
+              }
+              refs = {
+                type = "number"
+              }
+            }
+          }
+        }
+      }]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "traefikservices" {
+  manifest = {
+    apiVersion = "apiextensions.k8s.io/v1"
+    kind = "CustomResourceDefinition"
+    metadata = {
+      name = "traefikservices.traefik.containo.us"
+    }
+    spec = {
+      group = "traefik.containo.us"
+      scope = "Namespaced"
+      names = {
+        kind = "TraefikService"
+        plural = "traefikservices"
+        singular = "traefikservice"
+      }
+
+      versions = [{
+        name    = "v1alpha1"
+        served  = true
+        storage = true
+        schema = {
+          openAPIV3Schema = {
+            type = "object"
+            properties = {
+              data = {
+                type = "string"
+              }
+              refs = {
+                type = "number"
+              }
+            }
+          }
+        }
+      }]
+    }
+  }
+}
