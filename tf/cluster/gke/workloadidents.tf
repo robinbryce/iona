eec4f1d60c6b91072567eb96d3b5d18b6d512ce4
Robin Bryce <robinbryce@gmail.com>
2021-12-28 14:46:31 +0000

    add project_fqdn variable

--- tf/cluster/gke/workloadidents.tf
+++ tf/cluster/gke/workloadidents.tf
@@ -1,9 +1,15 @@
+locals {
+  # this is the workload identity base for the cluster. All workload identities
+  # are constructed from this - thats how they work.
+  workloadid_fqdn = "serviceAccount:${var.project}.svc.id.goog"
+}
+
 resource "google_service_account" "workloads" {
   for_each = {
     kubeip = ["kube-system", "kubeip-sa"]
   }
   account_id   = each.value[1]
-  display_name = substr("Workload Identity ${var.project_fqdn}[${each.value[0]}/${each.value[1]}]", 0, 100)
+  display_name = substr("Workload Identity ${local.workloadid_fqdn}[${each.value[0]}/${each.value[1]}]", 0, 100)
   project      = var.project
 }
 
@@ -14,5 +20,5 @@ resource "google_service_account_iam_member" "workloads" {
 
   service_account_id = google_service_account.workloads[each.key].name
   role               = "roles/iam.workloadIdentityUser"
-  member             = "serviceAccount:${var.project}.svc.id.goog[${each.value[0]}/${each.value[1]}]"
+  member             = "${local.workloadid_fqdn}[${each.value[0]}/${each.value[1]}]"
 }
