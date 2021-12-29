5e2063100c9b0f01cfae575134ff65bfded16e04
Robin Bryce <robinbryce@gmail.com>
2021-12-29 00:11:49 +0000

    adding traefik crds

--- tf/cluster-k8s/iona-traefik-service.tf
+++ tf/cluster-k8s/iona-traefik-service.tf
@@ -3,24 +3,24 @@ resource "kubernetes_service_v1" "traefik" {
     name = "traefik-web"
   }
   spec {
-    clusterIP = "None"
-    selector {
+    cluster_ip = "None"
+    selector = {
       "app.kubernetes.io/name" = "traefik-web"
     }
     port {
       name = "web"
       port = "80"
-      targetPort = "80"
+      target_port = "80"
     }
     port {
       name = "websecure"
       port = "443"
-      targetPort = "443"
+      target_port = "443"
     }
     port {
       name = "admin"
       port = "8080"
-      targetPort = "8080"
+      target_port = "8080"
     }
   }
 }
