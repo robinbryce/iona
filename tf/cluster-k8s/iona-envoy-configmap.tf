ec278c5fdba0be28e52ef8507341c08eb2a5309b
Robin Bryce <robinbryce@gmail.com>
2021-12-29 01:36:59 +0000

    adding envoy

--- tf/cluster-k8s/iona-envoy-configmap.tf
+++ tf/cluster-k8s/iona-envoy-configmap.tf
@@ -1,6 +1,7 @@
 resource "kubernetes_config_map_v1" "envoy" {
   metadata {
     name = "envoy-conf"
+    namespace = "${local.gcp_project_name}"
   }
   data = {
     "envoy.yaml" = "${file("${path.module}/iona-envoy.yaml")}"
