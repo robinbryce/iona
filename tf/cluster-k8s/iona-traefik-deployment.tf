b6bc306ce2bb333efc5d75cf936e014c1fa489af
Robin Bryce <robinbryce@gmail.com>
2021-12-29 00:49:22 +0000

    adding traefik deployment

---
+++ tf/cluster-k8s/iona-traefik-deployment.tf
@@ -0,0 +1,115 @@
+resource "kubernetes_deployment_v1" "traefik" {
+  metadata {
+    name = "traefik-web"
+    namespace = "${local.gcp_project_name}"
+    labels = {
+      "app.kubernetes.io/name" = "traefik-web"
+      "app.kubernetes.io/part-of" = "traefik-web"
+    }
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        "app.kubernetes.io/name" = "traefik-web"
+        "app.kubernetes.io/part-of" = "traefik-web"
+      }
+    }
+    strategy {
+      type = "Recreate"
+    }
+    template {
+      metadata {
+        labels = {
+          "app.kubernetes.io/name" = "traefik-web"
+          "app.kubernetes.io/part-of" = "traefik-web"
+        }
+      }
+      spec {
+        automount_service_account_token = true
+        container {
+            args = [
+              "-cx",
+              <<-EOT
+              set -e
+              traefik \
+                --api.insecure=true \
+                --entrypoints.web.address=:80 \
+                --entrypoints.websecure.address=:443 \
+                --entrypoints.ping.address=:10254 \
+                --ping.entrypoint=ping \
+                --log.level=DEBUG \
+                --providers.kubernetescrd \
+                --certificatesresolvers.letsencrypt.acme.dnschallenge=true \
+                --certificatesresolvers.letsencrypt.acme.dnschallenge.provider=gcloud \
+                --certificatesresolvers.letsencrypt.acme.caserver=https://acme-staging-v02.api.letsencrypt.org/directory \
+                --certificatesresolvers.letsencrypt.acme.email=$${ACME_EMAIL} \
+                --certificatesresolvers.letsencrypt.acme.storage=/var/run/acme/acme-staging.json
+              EOT
+            ]
+            command = [
+              "sh",
+            ]
+            env {
+              name = "ACME_EMAIL"
+              value = "noreply@${local.gcp_project_name}.thaumagen.io"
+            }
+            image = "traefik:latest"
+            name = "traefik"
+            liveness_probe {
+              failure_threshold = 3
+              http_get {
+                path = "/ping"
+                port = 10254
+                scheme = "HTTP"
+              }
+              initial_delay_seconds = 10
+              period_seconds = 10
+              success_threshold = 1
+              timeout_seconds = 1
+            }
+            port {
+              container_port = 80
+              name = "web"
+            }
+            port {
+              container_port = 443
+              name = "websecure"
+            }
+            port {
+              container_port = 8080
+              name = "admin"
+            }
+            readiness_probe {
+              failure_threshold = 3
+              http_get {
+                path = "/ping"
+                port = 10254
+                scheme = "HTTP"
+              }
+              period_seconds = 10
+              success_threshold = 1
+              timeout_seconds = 1
+            }
+            volume_mount {
+                mount_path = "/var/run/acme"
+                name = "acme-certs"
+            }
+        }
+        node_selector = {
+          "cloud.google.com/gke-nodepool" = "ingress-pool"
+        }
+        service_account_name = "dns01solver-sa"
+        toleration {
+          effect = "NoExecute"
+          key = "ingress-pool"
+          operator = "Equal"
+          value = "true"
+        }
+        volume {
+          name = "acme-certs"
+        }
+      }
+    }
+  }
+}
