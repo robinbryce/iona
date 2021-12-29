e92fb4b286f992a871e672d49f9789bc60312789
Robin Bryce <robinbryce@gmail.com>
2021-12-29 15:46:07 +0000

    move iona nginx static to authn as it uses the traefik crd's and those must be applied before tf can reference them

---
+++ tf/authn/iona-nginx-service.tf
@@ -0,0 +1,23 @@
+resource "kubernetes_service_v1" "iona-nginx" {
+  metadata {
+    name = "nginx"
+    namespace = "${local.cluster_namespace}"
+  }
+  spec {
+    type = "ClusterIP"
+    selector = {
+      "app.kubernetes.io/name" = "nginx"
+      "app.kubernetes.io/name" = "iona-authn"
+    }
+    port {
+      name = "tcp-80"
+      port = "80"
+      target_port = "80"
+    }
+    port {
+      name = "tcp-443"
+      port = "443"
+      target_port = "443"
+    }
+  }
+}
