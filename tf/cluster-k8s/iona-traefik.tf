7bb93c798e3091d3efde7f1cf0d75934bd267ce6
Robin Bryce <robinbryce@gmail.com>
2022-01-02 14:35:49 +0000

    adding helm deployed traefik

--- tf/cluster-k8s/iona-traefik.tf
+++ tf/cluster-k8s/iona-traefik.tf
@@ -1,17 +1,17 @@
-resource "helm_release" "traefik" {
-  name = "traefik"
-  namespace = "${local.gcp_project_name}"
-  chart = "traefik"
-  repository = "https://helm.traefik.io/traefik"
-
-  values = [
-    "${file("iona-traefik-values.yaml")}"
-  ]
-  set {
-    name = "additionalArguments"
-    value = "{--certificatesresolvers.letsencrypt.acme.email=${var.traefik_acme_email}}"
-  }
-}
+# resource "helm_release" "traefik" {
+#   name = "traefik"
+#   namespace = "${local.gcp_project_name}"
+#   chart = "traefik"
+#   repository = "https://helm.traefik.io/traefik"
+# 
+#   values = [
+#     "${file("iona-traefik-values.yaml")}"
+#   ]
+#   set {
+#     name = "additionalArguments"
+#     value = "{--certificatesresolvers.letsencrypt.acme.email=${var.traefik_acme_email}}"
+#   }
+# }
 
 resource "kubernetes_config_map_v1" "traefik-default-routes" {
   metadata {
