d9fbb4c6521c0937c6f1a9e880c46d714446a5b5
Robin Bryce <robinbryce@gmail.com>
2022-01-03 12:12:36 +0000

    switch back to e2-standard-2 instances due to N2 CPUs cap

--- tf/cluster/cluster.tf
+++ tf/cluster/cluster.tf
@@ -77,7 +77,7 @@ module "cluster" {
       service_account    = "kluster-serviceaccount@${var.gcp_project_id}.iam.gserviceaccount.com"
     }
     work-pool = {
-      machine_type       = "n2-standard-4" # $$$
+      machine_type       = "e2-standard-2" # $$$
       initial_node_count = 1
       min_node_count     = 1
       max_node_count     = 16
