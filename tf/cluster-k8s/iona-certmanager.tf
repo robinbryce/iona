62b4f31fa8f1b20b25ad818c67a52c8ac293b32e
Robin Bryce <robinbryce@gmail.com>
2022-01-03 10:40:45 +0000

    use the dns01solver-sa service account for all cert-manager service accounts

--- tf/cluster-k8s/iona-certmanager.tf
+++ tf/cluster-k8s/iona-certmanager.tf
@@ -8,4 +8,45 @@ resource "helm_release" "certmanager" {
     name  = "installCRDs"
     value = "true"
   }
+
+  # certmanager sa
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+  set {
+    name  = "serviceAccount.name"
+    value = "${google_service_account.dns["dns01solver"].account_id}"
+  }
+
+  # certmanager-webhook sa
+  set {
+    name  = "webhook.serviceAccount.create"
+    value = "false"
+  }
+  set {
+    name  = "webhook.serviceAccount.name"
+    value = "${google_service_account.dns["dns01solver"].account_id}"
+  }
+
+  # certmanager-cainjector sa
+  set {
+    name  = "cainjector.serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "cainjector.serviceAccount.name"
+    value = "${google_service_account.dns["dns01solver"].account_id}"
+  }
+
+  # certmanager-startupapicheck sa
+  set {
+    name  = "startupapicheck.serviceAccount.create"
+    value = "false"
+  }
+  set {
+    name  = "startupapicheck.serviceAccount.name"
+    value = "${google_service_account.dns["dns01solver"].account_id}"
+  }
 }
