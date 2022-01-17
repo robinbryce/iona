e89519dc59655ddbd3ecf56a4d680d7d01128152
Robin Bryce <robinbryce@gmail.com>
2022-01-17 08:34:31 +0000

    * Disable redis provider for traefik (using CRD's exclusively) * Disable google memorystore - its too expensive and its not being used*

--- tf/cluster-k8s/iona-traefik.tf
+++ tf/cluster-k8s/iona-traefik.tf
@@ -7,10 +7,16 @@ resource "helm_release" "traefik" {
   values = [
     "${file("iona-traefik-values.yaml")}"
   ]
+  # requires redis
+  #set {
+  #   name = "additionalArguments"
+  #   value = "{--api.insecure=true,--providers.file.directory=/etc/traefik/dynamic/default-routes,--providers.redis=true,--providers.redis.endpoints=${local.redis_memcache_endpoint},--certificatesresolvers.letsencrypt.acme.dnschallenge=true,--certificatesresolvers.letsencrypt.acme.dnschallenge.provider=gcloud,--certificatesresolvers.letsencrypt.acme.caserver=https://acme-staging-v02.api.letsencrypt.org/directory,--certificatesresolvers.letsencrypt.acme.email=${var.traefik_acme_email},--certificatesresolvers.letsencrypt.acme.storage=traefik/acme/account}"
+  #}
   set {
      name = "additionalArguments"
-     value = "{--api.insecure=true,--providers.file.directory=/etc/traefik/dynamic/default-routes,--providers.redis=true,--providers.redis.endpoints=${local.redis_memcache_endpoint},--certificatesresolvers.letsencrypt.acme.dnschallenge=true,--certificatesresolvers.letsencrypt.acme.dnschallenge.provider=gcloud,--certificatesresolvers.letsencrypt.acme.caserver=https://acme-staging-v02.api.letsencrypt.org/directory,--certificatesresolvers.letsencrypt.acme.email=${var.traefik_acme_email},--certificatesresolvers.letsencrypt.acme.storage=traefik/acme/account}"
+     value = "{--api.insecure=true,--providers.file.directory=/etc/traefik/dynamic/default-routes,--certificatesresolvers.letsencrypt.acme.dnschallenge=true,--certificatesresolvers.letsencrypt.acme.dnschallenge.provider=gcloud,--certificatesresolvers.letsencrypt.acme.caserver=https://acme-staging-v02.api.letsencrypt.org/directory,--certificatesresolvers.letsencrypt.acme.email=${var.traefik_acme_email},--certificatesresolvers.letsencrypt.acme.storage=traefik/acme/account}"
   }
+
 }
 
 resource "kubernetes_config_map_v1" "traefik-env" {
--- tf/cluster-k8s/memorystore-redis.tf
+++ tf/cluster-k8s/memorystore-redis.tf
@@ -1,12 +1,12 @@
-resource "google_redis_instance" "memcache" {
-  project            = "${local.gcp_project_id}"
-  region             = "${local.gcp_project_region}"
-  name               = "memcache"
-  memory_size_gb     = 1
-  connect_mode       = "PRIVATE_SERVICE_ACCESS"
-  authorized_network = local.vpc_network
-}
-
-locals {
-  redis_memcache_endpoint = "${google_redis_instance.memcache.host}:${google_redis_instance.memcache.port}"
-}
+# resource "google_redis_instance" "memcache" {
+#   project            = "${local.gcp_project_id}"
+#   region             = "${local.gcp_project_region}"
+#   name               = "memcache"
+#   memory_size_gb     = 1
+#   connect_mode       = "PRIVATE_SERVICE_ACCESS"
+#   authorized_network = local.vpc_network
+# }
+# 
+# locals {
+#   redis_memcache_endpoint = "${google_redis_instance.memcache.host}:${google_redis_instance.memcache.port}"
+# }
