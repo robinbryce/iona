7d69aa8d10e4f6308c4b6d4651eeed105e279cf5
Robin Bryce <robinbryce@gmail.com>
2022-03-13 19:44:18 +0000

    service account for gha from iona-app

--- tf/cluster/gke/github-actions-cd-workloadidentityfederation.tf
+++ tf/cluster/gke/github-actions-cd-workloadidentityfederation.tf
@@ -7,16 +7,31 @@ resource "google_service_account" "gh-oidc" {
   project      = var.project
 }
 
+resource "google_project_iam_member" "gh-oidc" {
+  project = var.project
+  role = "projects/${var.project}/roles/iam.workloadIdentityUser"
+  member = "principalSet://iam.googleapis.com/projects/871349271977/locations/global/workloadIdentityPools/github-oidc/attribute.repository/robinbryce/iona-app"
+}
+
+# XXX TODO need to depend on the sa above so it gets created first! applying
+# twice fixes it for now
 module "gh_oidc" {
   source      = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
   project_id  = var.project
   pool_id     = "github-oidc"
   provider_id = "github-provider"
   provider_description = "Workload Identity Pool Provider for GitHub Actions based CD. A service account exists for each enabled repository, named after that repostiory gha-cd-<repo>"
+  attribute_mapping = {
+    "google.subject": "assertion.sub",
+    "attribute.actor": "assertion.actor",
+    "attribute.aud": "assertion.aud",
+    "attribute.repository": "assertion.repository"
+  }
   sa_mapping = {
     "gha-cd-iona-app" = {
       sa_name   = "projects/${var.project}/serviceAccounts/gha-cd-iona-app@${var.project}.iam.gserviceaccount.com"
       attribute = "attribute.repository/robinbryce/iona-app"
+      # attribute = "*"
     }
   }
 }
