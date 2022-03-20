fcf6d76845f1a39aa19a64ac7e2d92fdf2aca940
Robin Bryce <robinbryce@gmail.com>
2022-03-20 08:53:06 +0000

    adding service account for repository

--- tf/cluster/gke/github-actions-cd-workloadidentityfederation.tf
+++ tf/cluster/gke/github-actions-cd-workloadidentityfederation.tf
@@ -1,16 +1,24 @@
-resource "google_service_account" "gh-oidc" {
-  for_each = {
+
+locals {
+  repositories = {
     iona_app = ["robinbryce", "iona-app"]
+    tokenator = ["robinbryce", "tokenator"]
   }
+}
+
+resource "google_service_account" "gh-oidc" {
+
+  for_each = local.repositories
   account_id   = "gha-cd-${each.value[1]}"
   display_name = substr("GitHub Actions ${each.value[0]}/${each.value[1]}", 0, 100)
   project      = var.project
 }
 
 resource "google_project_iam_member" "gh-oidc" {
+  for_each = local.repositories
   project = var.project
   role = "roles/iam.workloadIdentityUser"
-  member = "principalSet://iam.googleapis.com/projects/871349271977/locations/global/workloadIdentityPools/github-oidc/attribute.repository/robinbryce/iona-app"
+  member = "principalSet://iam.googleapis.com/projects/871349271977/locations/global/workloadIdentityPools/github-oidc/attribute.repository/robinbryce/${each.value[1]}"
 }
 
 resource "google_project_iam_custom_role" "imagepush" {
@@ -31,31 +39,10 @@ resource "google_project_iam_custom_role" "imagepush" {
 # insufficient to push the first image. (push the first one by hand or create a
 # special sa as storage.admin is very over powered)
 resource "google_project_iam_member" "gha-imagepush" {
+  for_each = local.repositories
+
   project = var.project
   role = "projects/${var.project}/roles/imagepush"
-  member = "serviceAccount:gha-cd-iona-app@${var.project}.iam.gserviceaccount.com"
+  member = "serviceAccount:gha-cd-${each.value[1]}@${var.project}.iam.gserviceaccount.com"
   depends_on = [google_project_iam_custom_role.imagepush]
-}
-
-# XXX TODO need to depend on the sa above so it gets created first! applying
-# twice fixes it for now
-module "gh_oidc" {
-  source      = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
-  project_id  = var.project
-  pool_id     = "github-oidc"
-  provider_id = "github-provider"
-  provider_description = "Workload Identity Pool Provider for GitHub Actions based CD. A service account exists for each enabled repository, named after that repostiory gha-cd-<repo>"
-  attribute_mapping = {
-    "google.subject": "assertion.sub",
-    "attribute.actor": "assertion.actor",
-    "attribute.aud": "assertion.aud"#,
-    #"attribute.repository": "assertion.repository"
-  }
-  sa_mapping = {
-    "gha-cd-iona-app" = {
-      sa_name   = "projects/${var.project}/serviceAccounts/gha-cd-iona-app@${var.project}.iam.gserviceaccount.com"
-      # attribute = "attribute.repository/robinbryce/iona-app"
-      attribute = "*"
-    }
-  }
 }
